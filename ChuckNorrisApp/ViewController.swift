af4b966e4e2f754ceccd0be93563ba6dc6bc3de0
Matheus Henrique Almeida <matheushenriquealmeida@MacBook-Pro-de-Matheus.local>
2024-03-04 14:46:32 -0300

    Done simple layout to help with tests

--- ChuckNorrisApp/ViewController.swift
+++ ChuckNorrisApp/ViewController.swift
@@ -9,17 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var askForJokeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        setButton()
+    }
+    
+    private func setButton() {
+        askForJokeButton.addTarget(self, action: #selector(askForJokeAction), for: .touchDown)
+    }
+    
+    @objc func askForJokeAction() {
         Task {
             if let joke = try? await ChuckNorrisWebClient().getJoke() {
                 print(joke)
+                myLabel.text = joke.value ?? ""
             }
         }
     }
-
-
 }
 
